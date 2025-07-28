c1954c9872500f7f1e390e027a806701f37a4709
xinyuan <songxinyuan@keyirobot.com>
2025-07-28 17:34:48 +0800

    ch2 有关运动学问题

--- src/ch2/motion.cc
+++ src/ch2/motion.cc
@@ -29,22 +29,25 @@ int main(int argc, char** argv) {
     }
 
     double angular_velocity_rad = FLAGS_angular_velocity * sad::math::kDEG2RAD;  // 弧度制角速度
-    SE3 pose;                                                                    // TWB表示的位姿
+    SE3 pose;                                                                    // TWB表示的位姿 也就是 齐次的[R|t;0 1]
     Vec3d omega(0, 0, angular_velocity_rad);                                     // 角速度矢量
     Vec3d v_body(FLAGS_linear_velocity, 0, 0);                                   // 本体系速度
     const double dt = 0.05;                                                      // 每次更新的时间
 
     while (ui.ShouldQuit() == false) {
         // 更新自身位置
-        Vec3d v_world = pose.so3() * v_body;
-        pose.translation() += v_world * dt;
+        Vec3d v_world = pose.so3() * v_body;   // 在自身坐标系下速度更新
+        pose.translation() += v_world * dt;    // 将这个更新转换到世界坐标系下
 
         // 更新自身旋转
         if (FLAGS_use_quaternion) {
+            // 对应的 公式 -2.78
+            // 四元数更新qExp[w] = q[1, 1/2 w] = q[1, 0.5wx, 0.5wy, 0.5wz] * dt 
             Quatd q = pose.unit_quaternion() * Quatd(1, 0.5 * omega[0] * dt, 0.5 * omega[1] * dt, 0.5 * omega[2] * dt);
-            q.normalize();
-            pose.so3() = SO3(q);
+            q.normalize();  // 归一化四元数 四元数是单位四元数 多次计算完完成之后可能不是单位四元数，所以需要归一化
+            pose.so3() = SO3(q);  // 将四元数转换为SO3
         } else {
+            // 公式2.55 旋转矩阵R(t) = R(t-1) * exp(omega * dt)
             pose.so3() = pose.so3() * SO3::exp(omega * dt);
         }
 
